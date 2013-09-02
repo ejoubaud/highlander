0e1a62f189f1976968664eb2712775a45497d3f6
ejoubaud <emmanuel.joubaud@envato.com>
2013-09-02 14:38:15 +1000

    Stop trying to use SSL

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,3 @@
-require 'openid/fetchers'
-OpenID.fetcher.ca_file = File.join(Rails.root,'config','curl.pem')
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_apps, domain: 'envato.com'
 end
