c72890b3323ddf00710952a69d875752b9876258
Sean St. Quentin <sean.st.quentin@envato.com>
2013-09-03 10:28:12 +1000

    Added metrics and badges for team city

--- db/data_migrations/20130903092850_add_team_city_metrics.rb
+++ db/data_migrations/20130903092850_add_team_city_metrics.rb
@@ -4,10 +4,20 @@ module DataMigration
 
       def up
         Metric.create!(name: 'team_city_build_complete', description: 'Build Complete')
+        Badge.create!(name: '1_green_build', tag: 'Well done, sir', description: 'First Green Build')
+        Badge.create!(name: '100_green_builds', tag: 'Green goblin centurion', description: '100 Green Builds')
+        Badge.create!(name: '250_green_builds', tag: 'Green goblin class act', description: '250 Green Builds')
+        Badge.create!(name: '500_green_builds', tag: 'Green goblin beast', description: '500 Green Builds')
+        Badge.create!(name: '1000_green_builds', tag: 'Green goblin GOD', description: '1000 Green Builds')
       end
 
       def down
         Metric.find_by_name('team_city_build_complete').destroy
+        Badge.where(name: "1_green_build").destroy
+        Badge.where(name: "100_green_builds").destroy
+        Badge.where(name: "250_green_builds").destroy
+        Badge.where(name: "500_green_builds").destroy
+        Badge.where(name: "1000_green_builds").destroy
       end
     end
   end
