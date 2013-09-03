64f897937edde7f1e91ba1489b4b3106924ea6fd
Sean St. Quentin <sean.st.quentin@envato.com>
2013-09-04 09:37:01 +1000

    Assign the user from the kinship for Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,8 @@ class Event < ActiveRecord::Base
 
   validate :value, numericality: true
 
+  before_save :ensure_user_setup
+
   Metric::NAMES.each do |metric|
     scope metric.pluralize.to_sym, -> { joins(:metric).where("metrics.name = '#{metric}'") }
   end
@@ -13,5 +15,11 @@ class Event < ActiveRecord::Base
   def self.with_key_and_value(key, value)
     where("data @> '#{key}=>\"#{value}\"'")
   end
+  private
+
+  def ensure_user_setup
+    self.user ||= kinship.user
+  end
+
 
 end
