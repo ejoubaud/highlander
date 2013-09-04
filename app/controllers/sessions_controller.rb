77c2206204b0bbf73a0d536aba2b3bb25d2eeaae
Sean St. Quentin <sean.st.quentin@envato.com>
2013-09-04 10:56:56 +1000

    Block non-envato emails

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,8 @@ class SessionsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
+    raise "Not an envato email" unless email.ends_with?("@envato.com")
     user = User.with_email(email) || User.create(name: name, hooroo_email: email, emails: [ email ])
-
-    # temporarily correct user emails so that hooroo_email is their Hooroo one
-    unless user.hooroo_email.ends_with?('@hooroo.com')
-      user.emails = (user.emails + [ user.hooroo_email ]).flatten.uniq
-      user.hooroo_email = email
-      user.save!
-    end
 
     session[:user_id] = user.id
     redirect_to root_url, notice: "Yay, <strong>#{user.name.split[0]}</strong>! You've successfully signed in to <strong>Hilander</strong>".html_safe
