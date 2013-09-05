bf697656c99e6c0005e92eac36ccdddc077d5cd2
Sean St. Quentin <sean.st.quentin@envato.com>
2013-09-05 15:11:27 +1000

    Bounty events weren't setting the kinship

--- app/observers/bounty_observer.rb
+++ app/observers/bounty_observer.rb
@@ -2,7 +2,8 @@ class BountyObserver < ActiveRecord::Observer
 
   def after_save bounty
     if bounty.claimed?
-      Event.create(metric: metric, user: bounty.claimed_by, value: bounty.reward, data: { tag: bounty.name })
+      kinship = bounty.clan.kinship_for_user(bounty.claimed_by)
+      Event.create(metric: metric, kinship: kinship, user: bounty.claimed_by, value: bounty.reward, data: { tag: bounty.name })
     end
   end
 
