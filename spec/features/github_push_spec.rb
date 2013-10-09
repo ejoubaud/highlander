8d525abe6cd6b1651412c1dad673fcdb2acc83dd
ejoubaud <emmanuel.joubaud@envato.com>
2013-10-09 15:12:10 +1100

    Fix push spec (clan support + username instead of email)

--- spec/features/github_push_spec.rb
+++ spec/features/github_push_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Github Push' do
 
-  given(:endpoint) { '/api/github_push.json' }
+  given(:endpoint) { "http://clan.#{SITE_ROOT}/api/github_push.json" }
 
   background do
     @first_time_badge       = FactoryGirl.create(:first_time)
@@ -10,16 +10,15 @@ feature 'Github Push' do
   end
 
   given!(:user)           { FactoryGirl.create(:user, :githubber) }
+  given!(:clan)           { FactoryGirl.create(:clan, slug: 'clan', users: [user]) }
   given!(:github_service) { user.service_for(:github) }
   given!(:metric)         { FactoryGirl.create(:github_push) }
 
-  given(:valid_params) do
-    { payload: GithubPayloads.push(github_service.emails.first) }
-  end
+  given(:github_payload) { GithubPayloads.push(github_service.username) }
 
   describe 'First push' do
 
-    background { page.driver.post endpoint, valid_params }
+    background { page.driver.post endpoint, payload: github_payload }
 
     scenario 'User is given the First Github push and First Timer badges' do
       visit user_path(user)
@@ -33,7 +32,7 @@ feature 'Github Push' do
 
   describe 'Second push' do
 
-    background { 2.times { page.driver.post endpoint, valid_params } }
+    background { 2.times { page.driver.post endpoint, payload: github_payload } }
 
     scenario 'User has more points' do
       visit user_path(user)
