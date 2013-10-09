ecb4e05364896e16f54f2fa7a05e679d010bf7d4
ejoubaud <emmanuel.joubaud@envato.com>
2013-10-09 14:48:57 +1100

    Fix Github issues feature specs

--- spec/features/github_issue_closed_spec.rb
+++ spec/features/github_issue_closed_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Github Issue Closed' do
 
-  given(:endpoint) { '/api/github_issue_closed.json' }
+  given(:endpoint) { "http://clan.#{SITE_ROOT}/api/github_issue_closed.json" }
 
   background do
     @first_time_badge                         = FactoryGirl.create(:first_time)
@@ -11,14 +11,15 @@ feature 'Github Issue Closed' do
   end
 
   given!(:user)           { FactoryGirl.create(:user, :githubber) }
+  given!(:clan)           { FactoryGirl.create(:clan, slug: 'clan', users: [user]) }
   given!(:github_service) { user.service_for(:github) }
   given!(:metric)         { FactoryGirl.create(:github_issue_closed) }
 
-  given(:valid_params)    { GithubPayloads.issue_closed(github_service.username) }
+  given(:github_payload)    { GithubPayloads.issue_closed(github_service.username) }
 
   describe 'First closed issue' do
 
-    background { page.driver.post endpoint, valid_params }
+    background { page.driver.post endpoint, payload: github_payload }
 
     scenario 'User is given the First Github Issue Closed and First Timer badges' do
       visit user_path(user)
--- spec/features/github_issue_opened_spec.rb
+++ spec/features/github_issue_opened_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Github Issue Opened' do
 
-  given(:endpoint) { '/api/github_issue_opened.json' }
+  given(:endpoint) { "http://clan.#{SITE_ROOT}/api/github_issue_opened.json" }
 
   background do
     @first_time_badge                         = FactoryGirl.create(:first_time)
@@ -11,14 +11,15 @@ feature 'Github Issue Opened' do
   end
 
   given!(:user)           { FactoryGirl.create(:user, :githubber) }
+  given!(:clan)           { FactoryGirl.create(:clan, slug: 'clan', users: [user]) }
   given!(:github_service) { user.service_for(:github) }
   given!(:metric)         { FactoryGirl.create(:github_issue_opened) }
 
-  given(:valid_params) { GithubPayloads.issue_opened(github_service.username) }
+  given(:github_payload) { GithubPayloads.issue_opened(github_service.username) }
 
   describe 'First opened issue' do
 
-    background { page.driver.post endpoint, valid_params }
+    background { page.driver.post endpoint, payload: github_payload }
 
     scenario 'User is given the First Github Issue Opened and First Timer badges' do
       visit user_path(user)
