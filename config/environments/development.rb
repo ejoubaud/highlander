db3ddefd8bc107b45584ff5fb0ed8cb4d183de13
Mario Cuba <mario.cuba@envato.com>
2016-06-20 22:19:19 +1000

    Make sure we use the host when generating URL's

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Highlander::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Make sure there's a defined host for URL's
-  config.action_mailer.default_url_options = { :host => SITE_ROOT }
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
